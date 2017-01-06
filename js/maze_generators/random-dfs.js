164ced5c70d40f2688ebb0759a9446d349eac953
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-01-06 01:02:21 -0500

    Add quotes around undefined when checking for type

--- js/maze_generators/random-dfs.js
+++ js/maze_generators/random-dfs.js
@@ -10,7 +10,7 @@ var _ = require('underscore')
  * @param {type} visited Object containing visited maze cells
  */
 var randomDfs = function (maze, position, visited) {
-  visited = typeof visited !== undefined ? visited : {}
+  visited = typeof visited !== 'undefined' ? visited : {}
 
   visited[getPositionString(position)] = true
 
